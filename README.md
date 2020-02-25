54a7fba43cf36d83a6c4f0ad59bddb6d7e36179c
akthrms <akt.hrms@gmail.com>
2020-02-26 00:50:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# conv-to-4-spaces
---
+++ content_script.js
@@ -0,0 +1,6 @@
+document
+  .getElementsByTagName("head")[0]
+  .insertAdjacentHTML(
+    "beforeend",
+    '<style type="text/css">* { tab-size: 4 !important; }</style>'
+  );
